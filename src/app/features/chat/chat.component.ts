64c366ebeedeaeabdccca3e43688c416575c1469
Luis David <jimeneziglesiasd@gmail.com>
2025-06-27 17:59:16 +0200

    Empezando la nueva IA | Starting the new AI

--- src/app/features/chat/chat.component.ts
+++ src/app/features/chat/chat.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewChecked, ElementRef, viewChild } from '@angular/core';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+import { CardModule } from 'primeng/card';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { CommonModule, NgClass, NgForOf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { SimpleMarkdownPipe } from '../../shared/pipes/markdown.pipe';
+
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [
+    CommonModule,
+    NgForOf,
+    NgClass,
+    FormsModule,
+    CardModule,
+    ScrollPanelModule,
+    InputTextModule,
+    ButtonModule,
+    ProgressSpinnerModule,
+    SimpleMarkdownPipe,
+  ],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+  styleUrl: './chat.component.css',
 })
-export class ChatComponent {
-
-}
+export class ChatComponent {}
